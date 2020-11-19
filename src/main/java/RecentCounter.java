2cc1c838900604fa83983df423f7ac2bef180a39
prasad7989 <you@example.com>
2020-11-19 10:38:24 +0530

    Comments

--- src/main/java/RecentCounter.java
+++ src/main/java/RecentCounter.java
@@ -3,19 +3,41 @@ import java.util.Queue;
 
 class RecentCounter {
 
+
+
     Queue<Integer> queue;
     public RecentCounter() {
         queue = new LinkedList<Integer>();
     }
 
+
+
+
     public int ping(int t) {
-        //looping till queue is empty 
-        while (!queue.isEmpty() && t - queue.peek() > 3000) {
-            queue.remove();
-        }
+
+//Add t to queue
 
         queue.add(t);
 
+//return current time to 3000 millisecond ago
+
+        while (queue.peek() < t - 3000)
+
+            queue.poll();
+
         return queue.size();
+
     }
+
+
+
 }
+
+
+/*
+
+class RecentCounterTest {
+    RecentCounter obj = new RecentCounter();
+    // Fetches the number of calls
+    System.out.println("number of calls    "+obj.ping(100));
+}*/
